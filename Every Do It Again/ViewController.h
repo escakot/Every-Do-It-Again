216231285599c0018818874cc414538043497fb2
escakot <ec@neorennie.com>
2017-07-19 21:06:44 -0400

    Initial Commit

---
+++ Every Do It Again/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Every Do It Again
+//
+//  Created by Errol Cheong on 2017-07-19.
+//  Copyright © 2017 Errol Cheong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Every Do It Again/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Every Do It Again
+//
+//  Created by Errol Cheong on 2017-07-19.
+//  Copyright © 2017 Errol Cheong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
